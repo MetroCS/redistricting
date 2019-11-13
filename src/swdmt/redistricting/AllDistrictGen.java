b92fc6d7cf2d9a2cdc74b3ba32d3f810e36550a3
Jessica Trujillo <51432752+Jessica-Trujillo@users.noreply.github.com>
2019-11-13 14:43:06 -0700

    CheckStyle Clean-up

--- src/swdmt/redistricting/AllDistrictGen.java
+++ src/swdmt/redistricting/AllDistrictGen.java
@@ -7,14 +7,13 @@ import java.util.ArrayList;
  *    region.
  *
  *
- * @author Jessica
+ * @author Jessica Trujillo
  * @version 1.0
  */
-public class AllDistrictGen
-{
+public class AllDistrictGen{
   
   /**
-  * Generates all possible districts in a given grid that are of a specific size
+  * Generates all possible districts in a given grid that are of a specific size.
   * @param gridWidth width of the grid
   * @param gridHeight height of grid
   * @param districtSize size of a single district
@@ -24,38 +23,35 @@ public class AllDistrictGen
      ArrayList<ArrayList<Location>> starPattern = StarGenerator.generatePattern(districtSize);
      
      ArrayList<District> currentDistricts = new ArrayList<District>();
-     for (int x = 0; x < gridWidth; x++){
-       for (int y = 0; y < gridHeight; y++){
-         for (ArrayList<Location> pattern : starPattern){
+     for (int x = 0; x < gridWidth; x++) {
+       for (int y = 0; y < gridHeight; y++) {
+         for (ArrayList<Location> pattern : starPattern) {
            ArrayList<Location> transformed = transformPattern(pattern, x, y);
-           if (!isDistrictInBounds(transformed, gridWidth, gridHeight)){
+           if (!isDistrictInBounds(transformed, gridWidth, gridHeight)) {
              continue;
            }
            
            District transformedAsDistrict = new District(transformed);
            
-           if (!Contains(currentDistricts, transformedAsDistrict)){
+           if (!Contains(currentDistricts, transformedAsDistrict)) {
              currentDistricts.add(transformedAsDistrict);
            }
          }
-         
        }
-       
      }
-  
      return currentDistricts;
   }
   
-    /**
+  /**
   * Returns Y/N on whether or not a similar district (district with same locations)
-  * exists inside the district collection passed in
+  * exists inside the district collection passed in.
   * @param districts list of districts
   * @param district the district to look for inside districts
   * @return whether or not a similar district was in the districts list
   */
-  private static boolean Contains(ArrayList<District> districts, District district){
-    for (District d : districts){
-      if (Equals(d, district)){
+  private static boolean Contains(ArrayList<District> districts, District district) {
+    for (District d : districts) {
+      if (Equals(d, district)) {
         return true;
       }
     }
@@ -63,24 +59,24 @@ public class AllDistrictGen
   }
 
   /**
-  * checks whether two districts have the same coordinates
+  * checks whether two districts have the same coordinates.
   * @param districtA first district to compare
   * @param districtB second district to compare
   * @return Whether or not districtA has the same Coordinates as districtB
   */
-  private static boolean Equals(District districtA, District districtB){
-    if (districtA.size() != districtB.size()){
+  private static boolean Equals(District districtA, District districtB) {
+    if (districtA.size() != districtB.size()) {
       return false;
     }
     
-    for (Location coordA : districtA.locations()){
+    for (Location coordA : districtA.locations()) {
       boolean anyMatch = false;
-      for (Location coordB : districtB.locations()){
-        if (coordA.equals(coordB)){
+      for (Location coordB : districtB.locations()) {
+        if (coordA.equals(coordB)) {
           anyMatch = true;
         }
       }
-      if (!anyMatch){
+      if (!anyMatch) {
         return false;
       }
     }
@@ -89,20 +85,19 @@ public class AllDistrictGen
 
   /**
   * Creates a new pattern based off of the passed in pattern, that is translated by
-  * X and Y
+  * X and Y.
   * @param pattern starting at the origin
   * @param offsetX the X translation value
   * @param offsetY the Y translation value
   * @return pattern translated by X and Y
   */  
-  private static ArrayList<Location> transformPattern(ArrayList<Location> pattern, int offsetX, int offsetY){
+  private static ArrayList<Location> transformPattern(ArrayList<Location> pattern, int offsetX, int offsetY) {
     ArrayList<Location> newPattern = new ArrayList<Location>();
     
-    for (Location coord : pattern){
+    for (Location coord : pattern) {
       Location transformedCoord = new Location(coord.xCoordinate() + offsetX, coord.yCoordinate() + offsetY);
       newPattern.add(transformedCoord);
     }
-    
     return newPattern;
   }
     
@@ -114,22 +109,21 @@ public class AllDistrictGen
   * @param gridHeigtht the upper-bound of Y axis
   * @return Whether district is in bounds or not
   */
-  private static boolean isDistrictInBounds(ArrayList<Location> district, int gridWidth, int gridHeight){
-    for (Location cord : district){
-      if (cord.xCoordinate() < 0)
+  private static boolean isDistrictInBounds(ArrayList<Location> district, int gridWidth, int gridHeight) {
+    for (Location cord : district) {
+      if (cord.xCoordinate() < 0) {
         return false;
-      if (cord.yCoordinate() < 0)
+      }
+      if (cord.yCoordinate() < 0) {
         return false;
-      
-      
-      if (cord.xCoordinate() >= gridWidth)
+      }
+      if (cord.xCoordinate() >= gridWidth) {
         return false;
-      if (cord.yCoordinate() >= gridHeight)
+      }
+      if (cord.yCoordinate() >= gridHeight) {
         return false;
-    }
-    
+      }
+    }   
     return true;
   }
-  
 }
-
