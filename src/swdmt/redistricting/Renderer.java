749077dab4aa0da597b9798dc78ad5679c78af71
Jody Paul <jody@acm.org>
2019-11-30 15:05:36 -0700

    Added implementation of Renderer class. Test coverage via RendererTest: instructions 97%; branches 83%

---
+++ src/swdmt/redistricting/Renderer.java
@@ -0,0 +1,154 @@
+package swdmt.redistricting;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.TreeMap;
+/**
+ * Utility class for rendering regions and districts.
+ * <p>Basic versions use ASCII text graphics.</p>
+ * <p>Enhanced versions use Java graphical interfaces.</p>
+ * <p>ASCII rendering specifies specific characters for party affiliation.
+ * <PRE>Symbol -- Meaning
+ * * -- Arbitrary content
+ * ? -- Affiliation unknown
+ * U -- Unaffiliated
+ * 0 -- Party 0
+ * 1 -- Party 1
+ * T -- Third Party
+ * </PRE></p>
+ * @author Dr. Jody Paul
+ * @version 20191128
+ */
+public final class Renderer {
+    /** Render of unknown cell content. */
+    private static final String ANY = "*";
+    /** Render of a corner. */
+    private static final String CORNER = "+";
+    /** Render of a vertical edge. */
+    private static final String EDGE = "|";
+    /** Render of the horizontal border of a cell. */
+    private static final String CELL_BORDER = "---+";
+    /** Render of the middle of an empty cell. */
+    private static final String CELL_EMPTY_MIDDLE = "   |";
+    /** Render of the middle of a cell with an associated resident. */
+    private static final String CELL_ANY_MIDDLE = " " + ANY + " |";
+    /** Table of party affiliation renderings. */
+    private static final List<Character> PARTY_LABELS;
+
+    static {
+        PARTY_LABELS = new ArrayList<Character>();
+        PARTY_LABELS.add('?'); // None
+        PARTY_LABELS.add('U'); // Unaffiliated
+        PARTY_LABELS.add('0'); // Party 0
+        PARTY_LABELS.add('1'); // Party 1
+        PARTY_LABELS.add('T'); // Any Third Party
+    }
+
+    /**
+     * Hide the constructor of this utility class.
+     */
+    private Renderer() { }
+
+    /**
+     * Renders a region as an ASCII-graphic string.
+     * The region is depicted as cells within a rectangular grid,
+     * with cells corresponding to locations in the region shown filled.
+     * @param showAffiliation true to show party affiliation; false otherwise
+     * @param region the region to be rendered
+     * @return the rendering of the given region
+     * @TODO render non-rectangular regions
+     */
+    public static String renderAsASCII(final boolean showAffiliation,
+                                       final Region region) {
+        String rendering = "";
+        // Return immediately if region is null or has no locations.
+        if (region == null || region.size() < 1) {
+            return rendering;
+        }
+
+        // Determine locations at x-coordinate and y-coordinate boundaries.
+        Collection<Location> locs = region.locations();
+        Location locWithMinimumX = locs
+                .stream()
+                .min((loc1, loc2) -> Integer.compare(loc1.xCoordinate(),
+                                                     loc2.xCoordinate()))
+                .orElse(null);
+        Location locWithMinimumY = locs
+                .stream()
+                .min((loc1, loc2) -> Integer.compare(loc1.yCoordinate(),
+                                                     loc2.yCoordinate()))
+                .orElse(null);
+        Location locWithMaximumX = locs
+                .stream()
+                .max((loc1, loc2) -> Integer.compare(loc1.xCoordinate(),
+                                                     loc2.xCoordinate()))
+                .orElse(null);
+        Location locWithMaximumY = locs
+                .stream()
+                .max((loc1, loc2) -> Integer.compare(loc1.yCoordinate(),
+                                                     loc2.yCoordinate()))
+                .orElse(null);
+
+        // Construct location:voter mapping.
+        Map<Location, Voter> voterMap = new TreeMap<>();
+        for (Voter vot : region.voters()) {
+            voterMap.put(vot.location(), vot);
+        }
+
+        int numRows = 1 + locWithMaximumY.yCoordinate()
+                        - locWithMinimumY.yCoordinate();
+        int numCols = 1 + locWithMaximumX.xCoordinate()
+                        - locWithMinimumX.xCoordinate();
+
+        // Render a row border.
+        String horizontalBorder = CORNER;
+        for (int i = 0; i < numCols; i++) {
+            horizontalBorder += CELL_BORDER;
+        }
+        horizontalBorder += "\n";
+
+        rendering = horizontalBorder;
+        for (int r = 0; r < numRows; r++) {
+            String rowMiddle = EDGE;
+            for (int c = 0; c < numCols; c++) {
+                Location currentLoc = new Location(c, r);
+                if (locs.contains(currentLoc)) {
+                    if (showAffiliation) {
+                        if (voterMap.keySet().contains(currentLoc)) {
+                            rowMiddle
+                                += CELL_ANY_MIDDLE
+                                     .replace('*',
+                                        PARTY_LABELS
+                                         .get(voterMap
+                                              .get(currentLoc)
+                                              .affiliation()
+                                              .ordinal()));
+                        } else {
+                            rowMiddle += CELL_ANY_MIDDLE;
+                        }
+                    } else {
+                        rowMiddle += CELL_ANY_MIDDLE;
+                    }
+                } else {
+                    rowMiddle += CELL_EMPTY_MIDDLE;
+                }
+            }
+            rowMiddle += "\n";
+            rendering += rowMiddle + horizontalBorder;
+        }
+
+        return rendering;
+    }
+
+    /**
+     * Renders a region as an ASCII-graphic string.
+     * Convenience method equivalent to
+     * <code>renderAsASCII(false, region)</code>.
+     * @param region the region to be rendered
+     * @return the rendering of the given region
+     */
+    public static String renderAsASCII(final Region region) {
+        return renderAsASCII(false, region);
+    }
+}
