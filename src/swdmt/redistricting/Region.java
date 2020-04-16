7e739fa47aa1048df1bcb76e49acdcd42050b4d7
Estrada303 <eestra13@msudenver.edu>
2020-04-15 19:38:39 -0600

    added test for creating new region method

--- src/swdmt/redistricting/Region.java
+++ src/swdmt/redistricting/Region.java
@@ -124,4 +124,16 @@ public class Region implements java.io.Serializable {
     public Set<Voter> voters() {
         return this.voters;
     }
+
+    /**
+    * @TODO Take in a set of voters and return the sideSize
+    * length needed to fit the voters into a RegionTest
+    * @param Set<Voter>
+    * @return length of side
+    */
+    public int convertToSquareRegion(final Set<Voter> Voters ) {
+        System.out.println("inConvertToSquareRegionMethod...");
+
+        return 0;
+    }
 }
