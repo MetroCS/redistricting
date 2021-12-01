d93df23834d9f120e2295ce7ac404565cbd13558
Kelzihar <90809462+Kelzihar@users.noreply.github.com>
2021-11-30 19:40:06 -0700

    RegionTest.java refactored to utilize JUnit 5 Jupiter and not JUnit 5 Vintage (#123)
    Includes updating of timeout and exception tests.

--- src/swdmt/redistricting/RegionTest.java
+++ src/swdmt/redistricting/RegionTest.java
@@ -1,16 +1,19 @@
 package swdmt.redistricting;
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.core.Is.is;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static java.time.Duration.ofMillis;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import java.util.Set;
 import java.util.HashSet;
 /**
  * Tests for class Region.
  *
  * @author  Dr. Jody Paul
- * @version 20191006
+ * @version 20211130
  */
 public class RegionTest {
     /** The maximum size region to test. */
@@ -29,7 +32,7 @@ public class RegionTest {
      *
      * Called before every test case method.
      */
-    @Before
+    @BeforeEach
     public void setUp() {
     }
 
@@ -38,10 +41,14 @@ public class RegionTest {
      *
      * Called after every test case method.
      */
-    @After
+    @AfterEach
     public void tearDown() {
     }
 
+    /**
+    * Test case for empty region sizes.
+    * Should return 0 with various empty or 0 inputs.
+    */
     @Test
     public void emptyRegionSizeTest() {
         assertThat((new Region()).size(), is(0));
@@ -49,6 +56,10 @@ public class RegionTest {
         assertThat((new Region(-0)).size(), is(0));
     }
 
+    /**
+    * Test case for empty number of voters in a region.
+    * Should return 0 with various empty or 0 inputs.
+    */
     @Test
     public void emptyRegionNumberOfVotersTest() {
         assertThat((new Region()).numberOfVoters(), is(0));
@@ -56,77 +67,129 @@ public class RegionTest {
         assertThat((new Region(-0)).numberOfVoters(), is(0));
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void squareRegionNegativeSizeTest() {
-        Region r = new Region(-4);
+    /**
+    * Test case for negative square regions.
+    * When a negative square region is created,
+    * throws IllegalArgumentException.
+    */
+    @Test
+    public void squareRegionNegativeSizeTest() throws IllegalArgumentException {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Region r = new Region(-4);
+        });
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void squareRegionNonSquareSizeTest() {
-        Region r = new Region(15);
+    /**
+    * Test case for non-square regions.
+    * When a new region is made with a non-square number,
+    * throws IllegalArgumentException
+    */
+    @Test
+    public void squareRegionNonSquareSizeTest() throws IllegalArgumentException {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Region r = new Region(15);
+        });
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void squareRegionNonSquareSizeTestNum3() {
-        Region r = new Region(3);
+    /**
+    * Test case for non-square regions, specifically with 3.
+    * When a new region is made with the number 3,
+    * throws IllegalArgumentException.
+    */
+    @Test
+    public void squareRegionNonSquareSizeTestNum3() throws IllegalArgumentException {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Region r = new Region(3);
+        });
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void squareRegionExtremeSizeTest() {
-        Region r = new Region(Integer.MAX_VALUE);
+    /**
+    * Test case for extreme region sizing.
+    * When a region is created of the max value,
+    * throws IllegalArgumentException.
+    */
+    @Test
+    public void squareRegionExtremeSizeTest() throws IllegalArgumentException {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Region r = new Region(Integer.MAX_VALUE);
+        });
     }
 
-    @Test(timeout = MAX_TIMEOUT)
+    /**
+    * Test case for valid region size.
+    * Should complete before 1000 milliseconds have passed.
+    */
+    @Test
     public void squareRegionValidSizeTest() {
-        int maxRegionSide = (int) Math.sqrt(MAX_REGION_SIZE);
-        for (int i = 0; i < maxRegionSide; i++) {
-            assertThat("Square region test failed at size " + i * i,
-                       i * i, is((new Region(i * i)).size()));
-        }
+        assertTimeout(ofMillis(MAX_TIMEOUT), () -> {
+            int maxRegionSide = (int) Math.sqrt(MAX_REGION_SIZE);
+            for (int i = 0; i < maxRegionSide; i++) {
+                assertThat("Square region test failed at size " + i * i,
+                           i * i, is((new Region(i * i)).size()));
+            }
+        });
     }
 
-    @Test(timeout = MAX_TIMEOUT)
+    /**
+    * Test case for number of voters in a square region.
+    * Should complete before 1000 milliseconds have passed.
+    */
+    @Test
     public void squareRegionNumberOfVotersTest() {
-        int maxRegionSide = (int) Math.sqrt(MAX_REGION_SIZE);
-        for (int i = 0; i < maxRegionSide; i++) {
-            assertThat("Square region number of voters failed at size " + i * i,
-                       (new Region(i * i)).numberOfVoters(), is(i * i));
-        }
+        assertTimeout(ofMillis(MAX_TIMEOUT), () -> {
+            int maxRegionSide = (int) Math.sqrt(MAX_REGION_SIZE);
+            for (int i = 0; i < maxRegionSide; i++) {
+                assertThat("Square region number of voters failed at size " + i * i,
+                           (new Region(i * i)).numberOfVoters(), is(i * i));
+            }
+        });
     }
 
-    @Test(timeout = MAX_TIMEOUT)
+    /**
+    * Test case for voters in location set.
+    * Should complete before 1000 milliseconds have passed.
+    */
+    @Test
     public void regionWithAllVotersInLocationSetTest() {
-        Location location1 = new Location(3, 3);
-        Location location2 = new Location(0, 1);
-        Voter voter1 = new Voter(Party.NONE, location1);
-        Voter voter2 = new Voter(Party.PARTY0, location2);
+        assertTimeout(ofMillis(MAX_TIMEOUT), () -> {
+            Location location1 = new Location(3, 3);
+            Location location2 = new Location(0, 1);
+            Voter voter1 = new Voter(Party.NONE, location1);
+            Voter voter2 = new Voter(Party.PARTY0, location2);
 
-        Set<Location> locationSet = new HashSet<Location>();
-        locationSet.add(location1);
-        locationSet.add(location2);
+            Set<Location> locationSet = new HashSet<Location>();
+            locationSet.add(location1);
+            locationSet.add(location2);
 
-        Set<Voter> voterSet = new HashSet<Voter>();
-        voterSet.add(voter1);
-        voterSet.add(voter2);
+            Set<Voter> voterSet = new HashSet<Voter>();
+            voterSet.add(voter1);
+            voterSet.add(voter2);
 
-        Region region = new Region(locationSet, voterSet);
-        assertThat("Region with all voters in location set test failed; Location and Voter sets of size 2 produced Region with wrong size.", region.size(), is(2));
-        assertThat("Region with all voters in location set test failed; Location and Voter sets of size 2 produced Region with wrong number of voters.", region.numberOfVoters(), is(2));
+            Region region = new Region(locationSet, voterSet);
+            assertThat("Region with all voters in location set test failed; Location and Voter sets of size 2 produced Region with wrong size.", region.size(), is(2));
+            assertThat("Region with all voters in location set test failed; Location and Voter sets of size 2 produced Region with wrong number of voters.", region.numberOfVoters(), is(2));
+        });
     }
 
-    @Test(timeout = MAX_TIMEOUT)
+    /**
+    * Test case for voters not in the location set.
+    * Should complete before 1000 milliseconds have passed.
+    */
+    @Test
     public void regionWithVoterNotInLocationSetTest() {
-        Location location3 = new Location(0,3);
-        Voter voter3 = new Voter(Party.NONE, new Location(0,2));
+        assertTimeout(ofMillis(MAX_TIMEOUT), () -> {
+            Location location3 = new Location(0, 3);
+            Voter voter3 = new Voter(Party.NONE, new Location(0, 2));
 
-        Set<Location> locationSet = new HashSet<Location>();
-        locationSet.add(location3);
+            Set<Location> locationSet = new HashSet<Location>();
+            locationSet.add(location3);
 
-        Set<Voter> voterSet = new HashSet<Voter>();
-        voterSet.add(voter3);
+            Set<Voter> voterSet = new HashSet<Voter>();
+            voterSet.add(voter3);
 
-        Region region = new Region(locationSet, voterSet);
-        assertThat("Region with no voters in the location set test failed; given Location set size 1 and Voter set size 1 produced Region with wrong size.", region.size(), is(1));
-        assertThat("Region with no voters in the location set test failed; given Location set size 1 and Voter set size 1 produced wrong number of voters in the region.", region.numberOfVoters(), is(0));
+            Region region = new Region(locationSet, voterSet);
+            assertThat("Region with no voters in the location set test failed; given Location set size 1 and Voter set size 1 produced Region with wrong size.", region.size(), is(1));
+            assertThat("Region with no voters in the location set test failed; given Location set size 1 and Voter set size 1 produced wrong number of voters in the region.", region.numberOfVoters(), is(0));
+        });
     }
 }
