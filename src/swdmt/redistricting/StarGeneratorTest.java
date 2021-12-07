fcb48f2e4e58ffa23a2abf0a3d7e422611d9259e
Lena Hamilton <hamiltonlena21@gmail.com>
2021-12-07 13:11:02 -0700

    Added StarGeneratorTest.java

---
+++ src/swdmt/redistricting/StarGeneratorTest.java
@@ -0,0 +1,35 @@
+package swdmt.redistricting;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import static org.junit.jupiter.api.Assertions.*;
+class StarGeneratorTest {
+    /**
+     * Ensure each pattern generated has the correct number of Locations,
+     * and there are no duplicates in any of the generated patterns.
+     */
+    @Test
+    void generatePatternTest() {
+        int n = 100;
+        ArrayList<ArrayList<Location>> patterns = StarGenerator.generatePattern(n);
+        for(ArrayList<Location> pattern: patterns) {
+            assertEquals(pattern.size(), n);
+            Set<Location> patternSet = new HashSet<>();
+            Location previousDistrict = null;
+            for(Location currentDistrict: pattern) {
+                if(previousDistrict != null) {
+                    assertTrue(
+                            currentDistrict.xCoordinate() + 1 == previousDistrict.xCoordinate() ||
+                                    currentDistrict.xCoordinate() - 1 == previousDistrict.xCoordinate() ||
+                                    currentDistrict.yCoordinate() + 1 == previousDistrict.yCoordinate() ||
+                                    currentDistrict.yCoordinate() - 1 == previousDistrict.yCoordinate()
+                    );
+                }
+                previousDistrict = currentDistrict;
+            }
+            pattern.forEach(location -> patternSet.add(location));
+            assertEquals(patternSet.size(), pattern.size());
+        }
+    }
+}
