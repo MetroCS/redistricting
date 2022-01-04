efcd9d859c7bbefc60ca61c2fdd3be82203d6af6
Jody Paul <jody@acm.org>
2022-01-03 19:06:06 -0700

    Updated API documentation using JDK16 and links to external APIs

--- docs/api/package-search-index.js
+++ docs/api/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"metrocs.redistricting"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"metrocs.redistricting"}];updateSearchResults();
