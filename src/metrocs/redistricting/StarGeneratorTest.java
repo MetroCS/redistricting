7d2ff968cb455a28b1b52ac85b301d2ecde33dbb
dsherm12MSU <dsherm12@msudenver.edu>
2022-04-27 16:12:00 -0600

    added StarGeneratorTest.java

---
+++ src/metrocs/redistricting/StarGeneratorTest.java
@@ -0,0 +1,31 @@
+package metrocs.redistricting;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.AnyOf.anyOf;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.ArrayList;
+/**
+ * Tests for Voter objects.
+ *
+ * @author Jada, David, Colin, John, Daniel, Ali
+ * @version
+ */
+public class StarGeneratorTest {
+    @Test
+    public void generatePatternTest() {
+        ArrayList<ArrayList<Location>> firstLocation = new ArrayList<ArrayList<Location>>();
+        ArrayList<ArrayList<Location>> secondLocation = new ArrayList<ArrayList<Location>>();
+        //check if method returns null
+        assertNotNull(StarGenerator.generatePattern(0));
+        //assign same parameter, test if same result
+        firstLocation = StarGenerator.generatePattern(4);
+        secondLocation = StarGenerator.generatePattern(4);
+        assertEquals(firstLocation,secondLocation);
+        //assign different parameter, test if different result
+
+    }
+
+}
