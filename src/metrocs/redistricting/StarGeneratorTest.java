b8cbb1e420e307fa1688e14c10e4fd797b9e7705
DanielShermanSchool <60718971+DanielShermanSchool@users.noreply.github.com>
2022-04-27 17:44:35 -0600

    Update StarGeneratorTest.java
    Removed ambiguous tests and variables

--- src/metrocs/redistricting/StarGeneratorTest.java
+++ src/metrocs/redistricting/StarGeneratorTest.java
@@ -1,11 +1,6 @@
 package metrocs.redistricting;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.AnyOf.anyOf;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
-import java.util.Set;
-import java.util.HashSet;
 import java.util.ArrayList;
 /**
  * Tests for Voter objects.
@@ -16,17 +11,9 @@ import java.util.ArrayList;
 public class StarGeneratorTest {
     @Test
     public void generatePatternTest() {
-        ArrayList<ArrayList<Location>> firstPattern = new ArrayList<ArrayList<Location>>();
-        ArrayList<ArrayList<Location>> secondPattern = new ArrayList<ArrayList<Location>>();
         //check if method returns null
         assertNotNull(StarGenerator.generatePattern(1));
         assertNotNull(StarGenerator.generatePattern(4));
-        //assign same parameter, test if same result
-        firstPattern = StarGenerator.generatePattern(4);
-        secondPattern = StarGenerator.generatePattern(4);
-        assertEquals(firstPattern,secondPattern);
-        //assign different parameter, test if different result
-
     }
 
 }
