e17d27dd4312c118a4e9b196db03ca5c65d153a6
ColinLouis95 <ckugler1@msudenver.edu>
2023-10-17 12:28:09 -0600

    Initial test class created for StarGenerator

---
+++ src/metrocs/redistricting/StarGeneratorTest.java
@@ -0,0 +1,38 @@
+package metrocs.redistricting;
+import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import org.junit.jupiter.api.Test;
+/**
+ * Tests for objects of type Location in Star Generator.
+ *
+ * @authors  Colin K, Jesus AE, Thomas M, Robert B
+ * @version 20231012.1
+ */
+
+class StarGeneratorTest{
+
+    /* Verifies that locations inside pattern generated are not empty */
+    @Test
+    public void StarGeneratorEmptyTest(){
+        ArrayList<ArrayList<Location>> sp = StarGenerator.generatePattern(3);
+        assertFalse(sp.isEmpty());
+    }
+
+    /* Verifies that the Star Generator method is not randomly generating a pattern */
+    @Test
+    public void StarGeneratorEqualTest(){
+        ArrayList<ArrayList<Location>> sp = StarGenerator.generatePattern(3);
+        ArrayList<ArrayList<Location>> sp2 = StarGenerator.generatePattern(3);
+        assertEquals(sp, sp2);
+    }
+    
+    /* Verifies that the intial location start point for the pattern is 0,0 */
+    @Test
+    public void StarGeneratorLocationStartTest(){
+        ArrayList<ArrayList<Location>> sp = StarGenerator.generatePattern(3);
+        Location l = new Location(0, 0);   
+        assertEquals(sp.get(0).toString(), l.xCoordinate(), l.yCoordinate());    
+    }
+
+}
