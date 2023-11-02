126293363a317f9172603921699b1ea1ec8b951b
ColinLouis95 <ckugler1@msudenver.edu>
2023-11-02 13:26:56 -0600

    update StarGeneratorTest methods

--- src/metrocs/redistricting/StarGeneratorTest.java
+++ src/metrocs/redistricting/StarGeneratorTest.java
@@ -1,6 +1,6 @@
 package metrocs.redistricting;
 import java.util.ArrayList;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import org.junit.jupiter.api.Test;
 /**
@@ -12,34 +12,22 @@ import org.junit.jupiter.api.Test;
 
 class StarGeneratorTest{
 
-    /** Verifies that locations inside pattern generated are not empty */
+    /** Verifies that locations inside pattern generated where n is >= 1 are not empty */
     @Test
     public void StarGeneratorEmptyTest(){
         ArrayList<ArrayList<Location>> sp = StarGenerator.generatePattern(3);
         assertFalse(sp.isEmpty());
     }
-
-    /** Verifies that the Star Generator method is not randomly generating a pattern */
-    @Test
-    public void StarGeneratorEqualTest(){
-        ArrayList<ArrayList<Location>> sp = StarGenerator.generatePattern(3);
-        ArrayList<ArrayList<Location>> sp2 = StarGenerator.generatePattern(3);
-        assertEquals(sp, sp2);
-    }
     
-    /** Verifies that the intial location start point for the pattern is 0,0 */
+    /** Verifies that for each set of locations, that it will contain the position (0,0) */
     @Test
     public void StarGeneratorLocationStartTest(){
         ArrayList<ArrayList<Location>> sp = StarGenerator.generatePattern(3);
         Location l = new Location(0, 0);   
         // assertEquals(sp.get(0).toString(), l.xCoordinate, l.yCoordinate);
-        assertEquals(sp.get(0).get(0).xCoordinate(), l.xCoordinate());   
-        assertEquals(sp.get(0).get(0).yCoordinate(), l.yCoordinate()); 
-    }
-
-    /** Verifies the StarGenerator utilites limit with Java's max integer value */
-    //@Test
-    public void StarGeneratorMaxN(){
-
+        for(int i = 0; i < sp.size(); i++){
+            assertTrue(sp.get(i).contains(l));
+        }
+         
     }
 }
